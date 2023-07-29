2ba5ebc66a0f593bd504d1dcc5ba2f1071f6e9ab
Rohith patel <rohithgithub999@gmail.com>
2023-07-29 08:58:54 +0000

    login and  changes in API 1 also

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ app.post("/register", async (requestObject, responseObject) => {
   */
   const { username, name, password, gender, location } = requestBody;
   const usernameQuery = `SELECT * FROM user WHERE username='${username}';`;
-  const dbResponse = await dbConnectionObject.run(usernameQuery);
-  //console.log(dbResponse); //{ stmt: Statement { stmt: undefined }, lastID: 0, changes: 0 }
-  //console.log(dbResponse.username); //undefined
-  if (dbResponse.username === undefined) {
+  const dbResponse = await dbConnectionObject.get(usernameQuery);
+  console.log(dbResponse); //output:undefined
+
+  if (dbResponse === undefined) {
     if (password.length < 5) {
       responseObject.status("400");
       responseObject.send("Password is too short");
@@ -60,5 +60,42 @@ app.post("/register", async (requestObject, responseObject) => {
   } else {
     responseObject.status(400);
     responseObject.send("User already exists");
+  }
+});
+
+//API 2:
+app.post("/login", async (requestObject, responseObject) => {
+  const requestBody = requestObject.body;
+  //console.log(requestBody); //{ username: 'adam_richard', password: 'richard_567' }
+  const { username, password } = requestBody;
+  const usernameQuery = `SELECT * FROM user WHERE username='${username}';`;
+  /*
+  const dbResponse = await dbConnectionObject.run(usernameQuery);
+  console.log(dbResponse); //output: { stmt: Statement { stmt: undefined }, lastID: 0, changes: 0 }
+  console.log(dbResponse.username); //output: undefined
+  
+  The reason why the dbResponse object returned by dbConnectionObject.run()
+  doesn't contain the user information directly is because the run() method
+  is typically used for executing queries that do not directly return data from
+  the database, such as INSERT, UPDATE, DELETE, or other non-SELECT queries.
+  */
+  const dbResponse = await dbConnectionObject.get(usernameQuery);
+  //console.log(dbResponse); //{username: 'adam_richard',name: 'Adam Richard',password: '$2b$10$ua5nQasozgZ2jYLtAQfRsusMvYdyuj/3lgM6yz9uCwZFVeN1Epfea', gender: 'male',location: 'Detroit'}
+  //console.log(dbResponse.username); //adam_richard
+  if (dbResponse.username !== undefined) {
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      dbResponse.password
+    );
+    if (isPasswordMatched) {
+      responseObject.status(200);
+      responseObject.send("Login success!");
+    } else {
+      responseObject.status(400);
+      responseObject.send("Invalid password");
+    }
+  } else {
+    responseObject.status(400);
+    responseObject.send("Invalid user");
   }
 });
